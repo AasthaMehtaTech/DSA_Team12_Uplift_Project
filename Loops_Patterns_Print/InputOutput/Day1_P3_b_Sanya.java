d11ccb5cc91a1ba22820ae4062bf2ebfc40cd8eb
Sanya Gupta <46195995+gsanya1003@users.noreply.github.com>
2020-06-08 16:29:51 +0530

    Day1_task

---
+++ Loops_Patterns_Print/InputOutput/Day1_P3_b_Sanya.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Solution
+{
+     public static void main(String args[])
+     {
+         Scanner x=new Scanner(System.in);
+        
+         int n = x.nextInt();
+         for(int i=1;i<=n;i++)//loop for new line
+         {   
+             for(int k=n-i;k>0;k--)//loop for printing spaces
+             {
+                System.out.print(" "); 
+             }
+             for(int j=1;j<=i;j++)//loop for printing "*"
+             {
+                 System.out.print("* ");
+             }
+             System.out.println();
+            
+         }
+         
+    }
+}
+          
+          
+         
+         
+
+              
+              
+          
+      
+           
+       
+          
+     
