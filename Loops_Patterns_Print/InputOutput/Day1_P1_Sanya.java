fa4fd1d454aded93c6c3cf0e4857a84fd1225c9e
Sanya Gupta <46195995+gsanya1003@users.noreply.github.com>
2020-06-08 16:07:56 +0530

    Create Day1_P1_Sanya.java
    Day 1 task

---
+++ Loops_Patterns_Print/InputOutput/Day1_P1_Sanya.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution
+{
+     public static void main(String args[])
+     {
+         Scanner x=new Scanner(System.in);
+        
+         int t = x.nextInt();
+         
+         System.out.println(t*t);
+              
+    }
+}
+          
+          
+         
+         
+
+              
+              
+          
+      
+           
+       
+          
+     
