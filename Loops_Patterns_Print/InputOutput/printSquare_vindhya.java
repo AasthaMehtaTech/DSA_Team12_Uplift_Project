a762760dbea895775a0a27f217831a6d54b79509
vindhya Hegde <50870871+vindhyasadanand@users.noreply.github.com>
2020-06-08 13:21:02 +0530

    Day 1 Solution

---
+++ Loops_Patterns_Print/InputOutput/printSquare_vindhya.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class printSquare_vindhya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n ;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		n = sc.nextInt();
+		 System.out.println("Square of number is :" + n*n);		
+
+	}
+
+}
