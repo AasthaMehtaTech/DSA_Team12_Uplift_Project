394d0a5d953045536706f33b2e44c0c781132a9c
samyukta20 <56357749+samyukta20@users.noreply.github.com>
2020-06-08 17:55:50 +0530

    Day1_P1_Samyukta.cpp

---
+++ Loops_Patterns_Print/InputOutput/Day1_P1_Samyukta.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int n,ans;  
+   cin>>n;    //enter number
+   ans=n*n;   //store answer
+   cout<<ans; //print answer
+    
+    return 0;
+}
