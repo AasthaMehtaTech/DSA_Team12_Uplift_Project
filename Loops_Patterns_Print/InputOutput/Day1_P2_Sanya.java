e005cfbec92aafb6dfc96643b6663fca8b93ddf1
Sanya Gupta <46195995+gsanya1003@users.noreply.github.com>
2020-06-08 16:12:50 +0530

    Day1_task

---
+++ Loops_Patterns_Print/InputOutput/Day1_P2_Sanya.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution
+{
+     public static void main(String args[])
+     {
+         Scanner x=new Scanner(System.in);
+        
+         int t = x.nextInt();
+         for(int i=1;i<11;i++)
+         {
+            System.out.println(t+" * "+i+" = "+t*i);
+         }
+         
+              
+    }
+}
+          
+          
+         
+         
+
+              
+              
+          
+      
+           
+       
+          
+     
