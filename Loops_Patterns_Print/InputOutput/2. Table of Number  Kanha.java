12fd7292acc76a785e77c3284064dd0b2371e568
KanhaMishra25 <59839730+KanhaMishra25@users.noreply.github.com>
2020-06-09 17:46:57 +0530

    Update 2. Table of Number  Kanha.java

--- Loops_Patterns_Print/InputOutput/2. Table of Number  Kanha.java
+++ Loops_Patterns_Print/InputOutput/2. Table of Number  Kanha.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class tableOfNumber_Debdyuti {
+public class tableOfNumber {
 	
 	public static void main(String args[]) {	
 		Scanner input=new Scanner(System.in);
