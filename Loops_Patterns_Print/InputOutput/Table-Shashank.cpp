d9fc3cbc72260851b639606b886ee112ea21b9f2
SHASHANK KESARWANI <52634856+amshashank@users.noreply.github.com>
2020-06-10 00:09:32 +0530

    Create Table-Shashank.cpp

---
+++ Loops_Patterns_Print/InputOutput/Table-Shashank.cpp
@@ -0,0 +1,16 @@
+//Printing a table
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int i,n;
+	cout<<"Enter any number\n:";
+	cin>>n;
+
+	for(i=1;i<=10;++i)
+	cout<<"\n"<<n<<" * "<<i<<" = "<<n*i;
+
+	return 0;
+}
