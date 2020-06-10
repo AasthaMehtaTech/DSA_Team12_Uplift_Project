c11a7bab5e13f14a50fb41446def340f8c9cd734
Sanya Gupta <46195995+gsanya1003@users.noreply.github.com>
2020-06-10 08:28:20 +0530

    Day3_task

---
+++ Loops_Patterns_Print/InputOutput/Day3_P3_Sanya.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Solution
+{
+     public static void main(String args[])
+     {
+         Scanner x=new Scanner(System.in);
+        
+         int n = x.nextInt();
+         int a=0,sum=0;
+         while(n>0)
+         {
+            a=x.nextInt();
+            sum+=a;
+            System.out.print(sum+" ");
+            n--;
+         }
+        
+    }
+}
+         
+         
+         
+
+              
+              
+          
+      
+           
+       
+          
+     
