3201b24a250e5e14168d9b868d13740008aff382
SHASHANK KESARWANI <52634856+amshashank@users.noreply.github.com>
2020-06-18 22:19:29 +0530

    Update Day-7--Merge2SortedArray-Shashank.cpp

--- Loops_Patterns_Print/InputOutput/Shashank/Day-7/Day-7--Merge2SortedArray-Shashank.cpp
+++ Loops_Patterns_Print/InputOutput/Shashank/Day-7/Day-7--Merge2SortedArray-Shashank.cpp
@@ -1,40 +1,8 @@
-#include<iostream> 
-using namespace std; 
-
-void mergeArrays(int arr1[], int arr2[], int n1, 
-                             int n2, int arr3[]) 
-{ 
-    int i = 0, j = 0, k = 0; 
+void Solution::merge(vector<int> &A, vector<int> &B) {
+   for(int i=0;i<B.size();i++)
+    {
+        A.push_back(B[i]);
+    }
     
-    while (i<n1 && j <n2) 
-    { 
-        if (arr1[i] < arr2[j]) 
-            arr3[k++] = arr1[i++]; 
-        else
-            arr3[k++] = arr2[j++]; 
-    } 
-  
-    while (i < n1) 
-        arr3[k++] = arr1[i++]; 
-
-    while (j < n2) 
-        arr3[k++] = arr2[j++]; 
-} 
- 
-int main() 
-{ 
-    int arr1[] = {1, 5, 8}; 
-    int n1 = sizeof(arr1) / sizeof(arr1[0]); 
-  
-    int arr2[] = {6, 9}; 
-    int n2 = sizeof(arr2) / sizeof(arr2[0]); 
-  
-    int arr3[n1+n2]; 
-    mergeArrays(arr1, arr2, n1, n2, arr3); 
-  
-    cout << "Modified A = "; 
-    for (int i=0; i < n1+n2; i++) 
-        cout << arr3[i] << " "; 
-  
-    return 0; 
-} 
+    sort(A.begin(),A.end());
+}
