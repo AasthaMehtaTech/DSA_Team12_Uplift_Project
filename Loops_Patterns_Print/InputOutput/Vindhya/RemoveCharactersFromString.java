d95287269d15dbc7a0eace59a464e262fca9b808
vindhya Hegde <50870871+vindhyasadanand@users.noreply.github.com>
2020-06-22 20:53:14 +0530

    Day 8 solution to first problem

---
+++ Loops_Patterns_Print/InputOutput/Vindhya/RemoveCharactersFromString.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Remove {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Strings");
+		String s1 = sc.next();
+		String s2 = sc.next();
+		removeele(s1,s2);
+	}
+  static void removeele(String s1,String s2)
+  {
+	  HashMap<Character,Integer> map = new HashMap<>();
+	  int size = s1.length();
+	  char y [] = s1.toCharArray();
+	 int i=0;
+	 while(i!=size)
+	 {
+		if(map.get(y[i])==null)
+		{
+			map.put(y[i],1);
+		}
+		else
+		{
+			int oldval = map.get(y[i]);
+			int newval = oldval + 1;
+			map.put(y[i], newval);
+		}
+		i++;
+	 }
+	 int j=0;
+	 char currKey;
+	 int size2 = s2.length();
+	 char x[] = s2.toCharArray();
+	 String res = "";
+	 while(j!=size2)
+	 {
+		 if(!map.containsKey(x[j]))
+		 {
+			 res = res + x[j];
+		 }
+		 j++;
+	 }
+	 System.out.println(res);
+  }
+}
