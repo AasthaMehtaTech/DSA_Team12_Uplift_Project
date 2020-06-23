d3968116f5372a6f94ddc8a99a66ba938dbb2d83
vindhya Hegde <50870871+vindhyasadanand@users.noreply.github.com>
2020-06-23 10:45:11 +0530

    Day 9 solution for second problem

---
+++ Loops_Patterns_Print/InputOutput/Vindhya/Reverse_Integer.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class reverse {
+	 
+		 static int reverse(int x) {
+		        long rev= 0;
+		        while( x != 0)
+		        {
+		            rev= rev*10 + x % 10;
+		            x= x/10;
+		            if( rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE)
+		                return 0;
+		        }
+		        return (int) rev;
+		    }
+	        
+	       
+	    
+
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+			System.out.println(reverse(n));
+
+	}
+
+}
