b7c38b3b9402f689b14ec67969287dfefd7d6b10
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-06-26 23:50:35 +0530

    Update P2_GenerateParentheses.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-11/P2_GenerateParentheses.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-11/P2_GenerateParentheses.cpp
@@ -1,1 +1,24 @@
-//yet to solve
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        backtrace(res, "", 0, 0, n);
+        return res;
+    }
+    
+private:
+    void backtrace(vector<string>& res, string str, int start, int end, int max) {
+        if(str.length() == max * 2) {
+            res.push_back(str);
+            return;
+        }
+        if(start < max) { // open parenthesis num is less than max
+            backtrace(res, str+"(", start+1, end, max);
+        }
+        if(end < start) {// close parenthesis num is less than open num
+            backtrace(res, str+")", start, end+1, max);
+        }
+    }
+    
+
+};
