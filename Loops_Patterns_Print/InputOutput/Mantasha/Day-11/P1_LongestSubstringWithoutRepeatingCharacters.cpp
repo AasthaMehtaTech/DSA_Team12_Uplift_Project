535a5d6d090f7ee85054260719d186c5502f9030
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-06-26 23:39:25 +0530

    Update P1_LongestSubstringWithoutRepeatingCharacters.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-11/P1_LongestSubstringWithoutRepeatingCharacters.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-11/P1_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,1 +1,16 @@
-//yet to solve
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    map<char, int> m;
+    int ans = 0, pos = -1;
+    for(int i = 0; i < s.size(); i++){
+      char x = s[i];
+      if (m.find(x) != m.end() && pos < m[x]) {
+        pos = m[x];
+      }
+      ans = max(i-pos, ans);
+      m[x] = i;
+    }
+    return ans;
+  }
+};
