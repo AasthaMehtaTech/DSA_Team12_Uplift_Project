a37071a8e79255a6bac6fff030f02e5dbb15a077
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-06-28 23:38:43 +0530

    Update P4_PairwiseSwapElementsOfALinkedList.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-12/P4_PairwiseSwapElementsOfALinkedList.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-12/P4_PairwiseSwapElementsOfALinkedList.cpp
@@ -1,1 +1,24 @@
+/*
+  Pairwise swap a linked list
+  The input list will have at least one element
+  node is defined as
+struct node
+{
+    int data;
+    struct node* next;
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}*head;
+*/
 
+Node* pairWiseSwap(struct Node* head) {
+    Node* p=head;
+   while(p!=NULL&&p->next!=NULL)
+   {
+       swap(p->data,p->next->data);
+       p=p->next->next;
+   }
+   return head;
+}
