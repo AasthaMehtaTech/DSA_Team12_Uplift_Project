5b5d9bc58911b15907b30c89eb8d98776fd5cdff
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-06-28 23:37:27 +0530

    Update P1_FindingMiddleElementInALinkedList.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-12/P1_FindingMiddleElementInALinkedList.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-12/P1_FindingMiddleElementInALinkedList.cpp
@@ -1,1 +1,29 @@
-//yet to solve
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+   int mid,n=0,c=1;
+   Node *p=head;
+   while(p!=NULL)
+   {
+       n++;
+       p=p->next;
+   }
+   mid=n/2+1;
+   while(c<mid){
+       c++;
+       head=head->next;
+   }
+   return head->data;
+}
