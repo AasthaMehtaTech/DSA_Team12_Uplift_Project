657f93f613e6b70b5597d6cac79189814e793e54
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-06-28 23:40:33 +0530

    Update P3_ReverseALlinkedList.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-12/P3_ReverseALlinkedList.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-12/P3_ReverseALlinkedList.cpp
@@ -1,1 +1,24 @@
-//yet to solve
+/* Linked List Node structure:
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+*/
+
+// Should reverse list and return new head.
+struct Node* reverseList(struct Node *head)
+{
+    Node *q,*p,*r;
+  r=NULL;
+  p=head;
+  q=NULL;
+  while(p!=NULL){
+    r=p->next;
+    p->next=q;
+    q=p;
+    p=r;
+    
+  }
+  return q;
+}
