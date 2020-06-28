99a1f56bc7939fd22594571f0b45f177d940a550
SHASHANK KESARWANI <52634856+amshashank@users.noreply.github.com>
2020-06-28 19:18:53 +0530

    Create Day-12--NthNode-Shashank.cpp

---
+++ Loops_Patterns_Print/InputOutput/Shashank/Day-12/Day-12--NthNode-Shashank.cpp
@@ -0,0 +1,33 @@
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+/* Should return data of n'th node from the end of linked list.
+*  head: head of the linked list
+*  n: nth node from end to find
+*/
+int getNthFromLast(Node *head, int n)
+{
+       Node *cur=head;
+       int i=0;
+       while(cur->next!=NULL)
+       {
+           cur=cur->next;
+           i++;
+       }
+       int count=i+1-n;
+       if(count<0)
+       return -1;
+       cur=head;
+       while(count--)
+       {
+           cur=cur->next;
+       }
+       return cur->data;
+}
