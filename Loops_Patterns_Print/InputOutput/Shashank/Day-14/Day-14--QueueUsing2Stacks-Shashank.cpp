3fb71f03c06268c96a8624c2f1aae1961f2eb74a
SHASHANK KESARWANI <52634856+amshashank@users.noreply.github.com>
2020-07-04 00:34:05 +0530

    Update Day-14--QueueUsing2Stacks-Shashank.cpp

--- Loops_Patterns_Print/InputOutput/Shashank/Day-14/Day-14--QueueUsing2Stacks-Shashank.cpp
+++ Loops_Patterns_Print/InputOutput/Shashank/Day-14/Day-14--QueueUsing2Stacks-Shashank.cpp
@@ -1,44 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
-void NGE(long int* arr, long int M);
+void StackQueue :: push(int x)
+ {          
+                s1.push(x);
+    
+ }
 
-
-
-void NGE(long int* arr, long int* aux, long int M)
-{
-    stack <long int> s;
-    for (long int i = 0; i < M; i++)
-    {
-        while (!s.empty() && arr[s.top()] < arr[i])
-        {
-            long int temp = s.top();
-            s.pop();
-            aux[temp] = arr[i];
+int StackQueue :: pop()
+{      int j;
+        
+        if(s1.empty()&&s2.empty())
+            return -1;
+        else if(!s1.empty()&&s2.empty())
+        {   while(!s1.empty())
+            {   s2.push(s1.top());
+                s1.pop();
+            }
+            j=s2.top();
+            s2.pop();
+            while(!s2.empty())
+            {   s1.push(s2.top());
+                s2.pop();
+            }
+            
+            return j;
         }
-        s.push(i);
-    }
-    for (long int j = 0; j < M; j++)
-    {
-    cout << aux[j] << " ";
-    }
-    cout << endl;
-}
-
-int main() {
-	long int N;
-	cin >> N;
-	for (long int i = 0; i < N; i++)
-	{
-	    long int M;
-	    cin >> M;
-	    long int arr[M];
-	    long int aux[M];
-	    for (long int j = 0; j < M; j++)
-	    {
-	        cin >> arr[j];
-	        aux[j] = -1;
-	    }
-	    NGE(arr, aux, M);
-	 }
-	return 0;
+            
 }
