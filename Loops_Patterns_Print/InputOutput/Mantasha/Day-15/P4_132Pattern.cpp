6128fec362ad3601fd5a8088d77522a4c79d2967
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-07-06 00:27:40 +0530

    Update P4_132Pattern.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-15/P4_132Pattern.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-15/P4_132Pattern.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        
+        int a_k = INT_MIN;
+        stack<int> stk;
+        
+        for(int i = nums.size()-1; i >= 0; i--) {
+            while(!stk.empty() && stk.top() < nums[i]) {
+                a_k = max(a_k,stk.top());
+                stk.pop();
+            }
+            
+            if(a_k != INT_MIN && a_k > nums[i]) return true;
+            
+            stk.push(nums[i]);
+        }
+    
+        return false;
+    }
+};
