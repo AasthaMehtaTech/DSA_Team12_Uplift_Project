5bb858440dad212ca80580de873b88b36d8efff0
SHASHANK KESARWANI <52634856+amshashank@users.noreply.github.com>
2020-07-05 22:24:07 +0530

    Add files via upload

---
+++ Loops_Patterns_Print/InputOutput/Shashank/Week-1/Day-1/Day-1--Square-Shashank.cpp
@@ -0,0 +1,12 @@
+// 1: Printing a Square
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the number ";
+    cin >> num;
+        cout << "Sqaure of "<< num << " is: " << num*num;
+    return 0;
+}
