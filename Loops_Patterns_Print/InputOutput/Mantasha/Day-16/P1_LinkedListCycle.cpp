194e5f764b5d0fb09a0bdbd26f8f2c4c5a960c9d
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-07-07 23:38:02 +0530

    Update P1_LinkedListCycle.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-16/P1_LinkedListCycle.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-16/P1_LinkedListCycle.cpp
@@ -1,1 +1,28 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        ListNode *p=head,*q=head;
+        if(head==NULL || head->next==NULL) return false;
+        
+        while(p){
+            p=p->next;
+            q=q->next;
+            if(p){
+                p=p->next;
+            }
+            if(p==q){
+                return true;
+            }
+        }
+        return false;
+    }
+};
