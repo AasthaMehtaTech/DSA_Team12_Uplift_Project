139ca83b4bbbf31d0b62bddcadbbdac4a0dd3696
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-07-09 15:22:25 +0530

    Update P2_Sub-arrayWithSumK.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-17/P2_Sub-arrayWithSumK.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-17/P2_Sub-arrayWithSumK.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    // time: O(n) and space: O(n)
+    int subarraySum(vector<int>& nums, int k) {
+        std::unordered_multiset<int> cum_sum; // O(n) space
+        cum_sum.insert(0); // initial sum;
+        int prev_sum = 0;
+        int total_sum = 0;
+    	for (int i = 0; i < nums.size(); i++) // O(n) time
+    	{
+            prev_sum += nums[i];
+            total_sum += cum_sum.count(prev_sum - k);
+            cum_sum.insert(prev_sum);
+    	}
+        return total_sum;
+    }
+};
