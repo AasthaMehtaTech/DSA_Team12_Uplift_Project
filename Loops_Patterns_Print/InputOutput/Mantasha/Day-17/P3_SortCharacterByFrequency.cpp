ec13fb10e39ae0ff7b1d2e786d4f623ed9c6e76e
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-07-09 15:23:59 +0530

    Update P3_SortCharacterByFrequency.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-17/P3_SortCharacterByFrequency.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-17/P3_SortCharacterByFrequency.cpp
@@ -1,1 +1,33 @@
-
+class Solution {
+public:
+    Solution(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+    static bool compare(pair<char,int>& a,pair<char,int>& b){
+        return (a.second > b.second);
+    }
+    string frequencySort(string s) {
+        unordered_map<char,int> m;
+        for(char c:s)
+            m[c]++;
+        vector<pair<char,int>> p;
+        for(auto it:m){
+            pair<char,int> temp;
+            temp.first=it.first;
+            temp.second=it.second;
+            p.push_back(temp);
+        }
+        sort(p.begin() , p.end() , compare);
+        string ans="";
+        for(auto it:p){
+            int num=it.second;
+            char q=it.first;
+            while(num--){
+                ans+=q;
+            }
+        }
+        return ans;
+    };
+};
