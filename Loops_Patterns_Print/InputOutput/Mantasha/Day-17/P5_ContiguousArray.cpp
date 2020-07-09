9ebcc701e25610cb202e691d8faddd33cd82827a
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-07-09 15:30:51 +0530

    Update P5_ContiguousArray.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-17/P5_ContiguousArray.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-17/P5_ContiguousArray.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+	int findMaxLength(vector<int>& nums) {
+  unordered_map<int,int> Map; 
+		Map[0]=-1;
+		int count=0;
+		int ans=0;
+		for(int i=0;i<nums.size();i++)
+		{
+			if(nums[i]==1)
+			{
+				count++;
+			}
+			else
+			{
+				count--;
+			}
+			if(Map.count(count))
+			{
+				ans=max(ans,i-Map[count]);
+			}
+			else
+			{
+				Map[count]=i;
+			}
+		}
 
+		return ans;
+	}
+};
