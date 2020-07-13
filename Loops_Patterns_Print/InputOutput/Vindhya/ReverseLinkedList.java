15efed1aaddc3514074a156e2077a24e27cd1a49
vindhya Hegde <50870871+vindhyasadanand@users.noreply.github.com>
2020-07-13 19:09:11 +0530

    Reversing of linkedlist

--- Loops_Patterns_Print/InputOutput/Vindhya/ReverseLinkedList.java
+++ Loops_Patterns_Print/InputOutput/Vindhya/ReverseLinkedList.java
@@ -6,7 +6,7 @@ class Node {
     }
 }
 
-*/
+
 
 class ReverseLL
 {
@@ -23,6 +23,36 @@ class ReverseLL
         current.next = prev;
     prev = current;
     current = head;
+    }
+        return prev;
+    }
+}
+/**********Second approach********************/
+class Node {
+    int value;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+
+
+class ReverseLL
+{
+    // This function should reverse linked list and return
+    // head of the modified linked list.
+    Node reverseList(Node head)
+    {
+      Node current = head;
+    Node prev = null;
+     Node nextnode = head;
+    while(nextnode!=null)
+    {
+        nextnode = nextnode.next;
+        current.next = prev;
+        prev = current;
+        current = nextnode;
     }
         return prev;
     }
