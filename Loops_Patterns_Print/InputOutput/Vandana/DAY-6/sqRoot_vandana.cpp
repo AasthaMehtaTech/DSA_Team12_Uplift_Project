b33d4039a28c3c5be4e6bbd2bd6baed2e6e45458
vandanaguk <55634048+vandanaguk@users.noreply.github.com>
2020-07-21 19:59:54 +0530

    square root

---
+++ Loops_Patterns_Print/InputOutput/Vandana/DAY-6/sqRoot_vandana.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number\n";
+    cin>>n;
+    cout<<floor(sqrt(n));
+}
+
