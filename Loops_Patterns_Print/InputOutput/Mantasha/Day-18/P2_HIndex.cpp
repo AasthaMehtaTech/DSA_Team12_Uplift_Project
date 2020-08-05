c9e0779714ff3fc4a6eb81de649b111ea7ed2655
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-08-05 22:46:33 +0530

    Update P2_HIndex.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-18/P2_HIndex.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-18/P2_HIndex.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+	int hIndex(vector<int>& citations) {
+		const int n = citations.size();
+		vector<int> hindex(n+1);
+		for (int i = 0; i < n; ++i)
+			hindex[n-min(citations[i], n)]++;
 
+		partial_sum(begin(hindex), end(hindex), begin(hindex), plus<int>());
+
+		for (int i = 0; i <= n; ++i) {
+			int h = n - i;
+			if (hindex[i] >= h)
+				return h;
+		}
+
+		return 0;
+	}
+};
