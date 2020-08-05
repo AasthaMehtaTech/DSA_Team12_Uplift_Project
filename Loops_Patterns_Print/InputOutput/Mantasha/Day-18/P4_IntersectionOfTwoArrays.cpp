1ae2b190ac84af0e3fc6435341af3929a5b8f765
MANTASHA SHAKEEL <62848573+ammantasha@users.noreply.github.com>
2020-08-05 22:49:49 +0530

    Update P4_IntersectionOfTwoArrays.cpp

--- Loops_Patterns_Print/InputOutput/Mantasha/Day-18/P4_IntersectionOfTwoArrays.cpp
+++ Loops_Patterns_Print/InputOutput/Mantasha/Day-18/P4_IntersectionOfTwoArrays.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    set<int> s1 , s2;
+    int i;
+    for( i=0 ; i<nums1.size() ; i++)
+        s1.insert( nums1[i] );
+    for( i=0 ; i<nums2.size() ; i++ )
+        s2.insert( nums2[i] );
+    vector<int> vec;   
+    for ( auto i : s1)
+    {   if( s2.find(i) != s2.end() )  
+        vec.push_back( i ) ;
+    }
+    return vec ;
+}
+};
