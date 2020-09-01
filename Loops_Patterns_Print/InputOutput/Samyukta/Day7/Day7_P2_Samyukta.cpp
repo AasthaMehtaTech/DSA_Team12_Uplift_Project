4155f035f897773e62e6391aebf2c89077b88188
samyukta20 <56357749+samyukta20@users.noreply.github.com>
2020-09-01 12:49:34 +0530

    FindMissingNumber

---
+++ Loops_Patterns_Print/InputOutput/Samyukta/Day7/Day7_P2_Samyukta.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int i;
+        sort(nums.begin(),nums.end());
+        for(i=0;i<nums.size();i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                break;
+            }
+        }
+        return nums[i];
+    }
+};
