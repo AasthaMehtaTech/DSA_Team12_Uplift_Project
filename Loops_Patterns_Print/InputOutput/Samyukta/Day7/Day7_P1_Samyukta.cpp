598fb4565e1c433e653be4fa1248b6e4a4ed7d1b
samyukta20 <56357749+samyukta20@users.noreply.github.com>
2020-09-01 12:07:14 +0530

    MissingNumber

--- Loops_Patterns_Print/InputOutput/Samyukta/Day7/Day7_P1_Samyukta.cpp
+++ Loops_Patterns_Print/InputOutput/Samyukta/Day7/Day7_P1_Samyukta.cpp
@@ -14,3 +14,26 @@ int main() {
 	cout<<actual-sum;
 	return 0;
 }
+
+
+
+//2 approch not including 0
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,n,sum=0;
+	cin>>n;
+	int arr[n];
+	int actual=(n+1)*(n+2)/2;               //max number is n+1 as 0 is not included and array is of n
+	for(i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	    sum=sum+arr[i];
+	}
+	cout<<actual-sum;
+	return 0;
+}
