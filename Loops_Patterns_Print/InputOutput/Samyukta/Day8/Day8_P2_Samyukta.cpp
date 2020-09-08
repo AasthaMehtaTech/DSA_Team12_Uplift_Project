c489cfb81698095eb1740f19bc1296a48587209e
samyukta20 <56357749+samyukta20@users.noreply.github.com>
2020-09-08 16:31:01 +0530

    RemoveDuplicate

---
+++ Loops_Patterns_Print/InputOutput/Samyukta/Day8/Day8_P2_Samyukta.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {                   //needs to be efficient
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    cin>>s;
+	    int i,n=s.length();
+	    int temp[123]={0};
+	    for(i=0;i<n;i++)
+	    {
+	        temp[(int)s[i]]++;
+	        if(temp[(int)s[i]]<2)
+	        {
+	            cout<<s[i];
+	        }
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
