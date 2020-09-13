bda2b0bfac700a3fe6ef7c45f157064168c6ca15
samyukta20 <56357749+samyukta20@users.noreply.github.com>
2020-09-13 19:57:44 +0530

    Valid Palindrome

---
+++ Loops_Patterns_Print/InputOutput/Samyukta/Day9/Day9_P2_Samyukta.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0,j=s.length()-1;
+        while(i<=j)
+        {
+            if(!isalnum(s[i]))
+            {
+                i++;
+            }
+            else if(!isalnum(s[j]))
+            {
+                j--;
+            }
+            else if(tolower(s[i])==tolower(s[j]))
+            {
+                i++;
+                j--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+};
